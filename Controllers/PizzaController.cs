c3a00508775397e8f3a7831443a99a95c4253147
Avisalon <bamboocho007@gmail.com>
2023-10-13 19:13:52 +0300

    init

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaParty.Models;
+
+namespace PizzaParty.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    [HttpGet(Name = "GetPizzaList")]
+    public IEnumerable<Pizza> Get()
+    {
+        return new List<Pizza>();
+    }
+
+    [HttpPost(Name = "CreateNewPizza")]
+    public async Task<Pizza> Post()
+    {
+        await Task.Delay(1000);
+        return new Pizza();
+    }
+
+    [HttpDelete(Name = "Delete pizza")]
+    public async Task Delete()
+    {
+        await Task.Delay(1000);
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaParty.Models;
+
+public class Pizza
+{
+    Guid Id { get; set; }
+    string Name { get; set; } = string.Empty;
+    string Description { get; set; } = string.Empty;
+    float Price { get; set; }
+}
